4a8c4ea229a5c26841f90d18fc4f0177047cb71b
klaus-i-am <kerwin.jackson14@gmail.com>
2025-04-15 22:23:57 -0700

    Variables

---
+++ Variables/index.js
@@ -0,0 +1,18 @@
+// variable = A container that stores a value. 
+//            Behaves as if it were the value it contains.
+
+// 1. Declaration = let x; <- A box that doesn't have anything in it yet.
+// 2. Assignment = x = 100; <- Now it has something with a value of 100 in it. 
+
+// Boolean = true or false. 
+
+let name;
+name = "Klaus";
+
+let age;
+age = 28;
+
+let isHuman = false;
+
+console.log(typeof isHuman);
+console.log(`Hello, ${name}! I see you are ${age} years old. Congratulations!`);
