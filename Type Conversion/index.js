b92f5c174e9761707f4cfd1fe33a511396a6036f
klaus-i-am <kerwin.jackson14@gmail.com>
2025-04-17 19:52:42 -0700

    Type Conversion

---
+++ Type Conversion/index.js
@@ -0,0 +1,21 @@
+// type conversion = changes the datatype of a value to another 
+//                   (strings, numbers, booleans)
+
+
+// let age = window.prompt("How old are you?");
+
+// age = Number(age);
+
+// age += 1;
+
+// console.log(age);
+
+let x,y,z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
