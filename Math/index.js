b4b38c02184efd890d4cfa3d4c03d7e1ef4ba834
klaus-i-am <kerwin.jackson14@gmail.com>
2025-04-17 22:22:12 -0700

    Math

---
+++ Math/index.js
@@ -0,0 +1,23 @@
+// Math = built-in object that provides a collection of properties and methods 
+
+let x = 3;
+let y = 2;
+let z;
+
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(x, y);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+// z = Math.sign(x);
+// z = Math.max(x, y, z);
+// z = Math.min(x, y, z);
+
+
+console.log(z);
