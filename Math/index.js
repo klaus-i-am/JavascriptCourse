8684b7af4d2b275d0f4635337dfe09363eba5feb
klaus-i-am <kerwin.jackson14@gmail.com>
2025-04-18 23:22:54 -0700

    Functions

--- Math/index.js
+++ Math/index.js
@@ -1,8 +1,8 @@
 // Math = built-in object that provides a collection of properties and methods 
 
-let x = 3;
-let y = 2;
-let z;
+// let x = 3;
+// let y = 2;
+// let z;
 
 // z = Math.round(x);
 // z = Math.floor(x);
@@ -18,6 +18,80 @@ let z;
 // z = Math.sign(x);
 // z = Math.max(x, y, z);
 // z = Math.min(x, y, z);
+// console.log(z);
+
+// IF STATEMENTS = if a condition is true, execute some code
+//                 if not, do something else
+
+let age = 15; 
+let isHuman = true;
+
+// if (age >= 18) {
+//     console.log("Are you 18 years old?");
+// } else {
+//     console.log("Nah..fam, you must be 18+ years old to enter!");
+//     console.log("Back to the lobby!");
+// }
+
+// if (isHuman) {
+//     console.log("You are Human!");
+// } else {
+//     console.log("W-what are you?!");
+// }
+
+// .checked -> for radio buttons, evaluates true or false.
 
 
-console.log(z);
+// /* Switches */ ========
+
+// let choice = "yes";
+
+// switch(choice) {
+//     case "yes": 
+//         console.log("Great!");
+//         break;
+//     case "no": 
+//         console.log("Too bad!");
+//     default: 
+//         console.log(`${choice} is not a choice!`);
+//         break;
+// }
+
+// String methods = allow you to manipulate and work with text (strings)
+    // .repeat(), .toUpperCase(), .replaceAll(), .slice(start, end), 
+
+
+// let userName = " Klaus";
+
+// let result = userName.padStart(15, "=");
+
+// console.log(result);
+
+// ========= Logical Operators ==========
+// = assignment operator
+// == comparison operator (compare if values are equal)
+// === strict equality operator (compare if values & datatype are equal)
+// != inequality operator 
+// !== strict inequality operator
+
+// ==== Loops =====
+
+// while loop
+// continuous loop
+
+// let isOnline = true;
+
+// while(isOnline) {
+//     console.log("User online!");
+//     isOnline = false;
+//     console.log(isOnline);
+// }
+// example could be a "log in" screen.
+
+
+// ===== for loop =====
+// limited loop = discrete
+ 
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Slime defeated! +${i + 1}exp gained!`);
+// }
---
+++ Math/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let isOnline = true;
+
+// while(isOnline) {
+//     console.log("User online!");
+//     isOnline = false;
+//     console.log(isOnline);
+// }
