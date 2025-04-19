fce9b4eef384b01ec006e15cd795527bdc95418a
klaus-i-am <kerwin.jackson14@gmail.com>
2025-04-18 23:42:10 -0700

    spread operator

--- Math/index.js
+++ Math/index.js
@@ -23,8 +23,8 @@
 // IF STATEMENTS = if a condition is true, execute some code
 //                 if not, do something else
 
-let age = 15; 
-let isHuman = true;
+// let age = 15; 
+// let isHuman = true;
 
 // if (age >= 18) {
 //     console.log("Are you 18 years old?");
@@ -95,3 +95,40 @@ let isHuman = true;
 // for (let i = 0; i < 10; i++) {
 //     console.log(`Slime defeated! +${i + 1}exp gained!`);
 // }
+
+// ==== Functions ====
+
+// let name = "Klaus";
+
+// function greetings(name) {
+//     console.log(`Hello, ${name}!`);
+// }
+// greetings(name);
+
+
+// ==== Arrays ====
+
+// let numbers = [1,2,3,4,5,6];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+
+// === Spread operator ====
+// spread operator = "..." allows an iterable such as an array or string to be expanded into separate elements (unpacks the elements -> items in the array). ~ Imagine opening a box and taking out what's inside then you can do whatever you want with it.
+
+// let numbers = [1,2,3,4,5];
+// let max = Math.max(...numbers);
+// let min = Math.min(...numbers);
+// console.log(max);
+// console.log(min);
+
+// let username = "klaus";
+// let letters = [...username];
+// // makes "klaus" -> ['k','l','a','u','s']
+
+// // join them back together
+// let letter = [...username].join('-');
+
+// console.log(letter);
